a909eaa75e12b02cc469b2d2bba560fec5ab1c67
Masaki Hidano <masagon@info-lounge.jp>
2017-05-04 21:12:37 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# s3-basic-auth-proxy
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+Frontend proxy server with Basic Authentication for Amazon S3 written by Node.js.
+
+## Getting Started
+
+    $ git clone git@github.com:InfoLoungeLLC/s3-basic-auth-proxy.git
+    $ cd s3-basic-auth-proxy
+    $ npm install
+    $ AWS_ACCESS_KEY_ID=<id> \
+    > AWS_SECRET_ACCESS_KEY=<secret> \
+    > S3_REGION=<region> \
+    > S3_BUCKET=<bucket> \
+    > AUTH_USER=<user> \
+    > AUTH_PASSWORD=<password> \
+    > npm start
+
+Then use http://localhost:3001/ instead of http://bucket.s3.amazon.com/.
+
+## Environment variables
+- PORT : Port of Proxy Server (default: 3001)
+- AWS_ACCESS_KEY_ID : AWS Access Key ID (required)
+- AWS_SECRET_ACCESS_KEY : AWS Secret Access Key (required)
+- S3_REGION : S3 region (default: ap-northeast-1)
+- S3_BUCKET : S3 Bucket (required)
+- AUTH_USER : Basic Auth Username (required)
+- AUTH_PASSWORD : Basic Auth Password (required)
---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+app.set('port', process.env.PORT || 3001)
+
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+  region: process.env.S3_REGION || 'ap-northeast-1'
+})
+
+const basicUser = process.env.AUTH_USER || ''
+const basicPassword = process.env.AUTH_PASSWORD || ''
+
+if (!basicUser || !basicPassword) {
+  console.log('AUTH_USER and AUTH_PASSWORD is required.')
+  process.exit()
+}
+const expectedAuthHeader = 'Basic ' + Buffer.from(basicUser + ':' + basicPassword, 'ascii').toString('base64')
+
+app.get('*', (req, res) => {
+  const authHeader = req.get('Authorization') || undefined
+  if (authHeader && expectedAuthHeader === authHeader) {
+    // Basic Auth Successed
+    const params = {
+      Bucket: process.env.S3_BUCKET || '',
+      Key: req.url.substring(1) || 'index.html'
+    }
+    s3.getObject(params).createReadStream()
+      .on('error', () => {
+        res.status(404).send('Not Found')
+      })
+      .pipe(res)
+  } else {
+    // Basic Auth Failed
+    res.append('WWW-Authenticate', 'Basic realm="Namie Admin Console"')
+    res.status(401).send('Unauthorized')
+  }
+})
+
+app.listen(app.get('port'), () => {
+  console.log('NamieAdminProxy is listening on port ' + app.get('port'))
+})
